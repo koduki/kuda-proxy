0cdec8caf6aeaa9f09f80bdd21d7788bed4818e6
koduki <pascalm3+git@gmail.com>
2021-03-15 19:56:33 +0000

    Add healthcheck

--- src/main/java/app/ProxyResource.java
+++ src/main/java/app/ProxyResource.java
@@ -16,6 +16,7 @@ import java.util.stream.Collectors;
 import javax.inject.Inject;
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -47,6 +48,12 @@ public class ProxyResource {
 
     @Inject
     HttpServletRequest forwardRequest;
+    
+    @GET
+   @Path("/healthcheck")
+    public String healthcheck() {
+        return targetName + ":" + targetUrl;
+    }
 
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
